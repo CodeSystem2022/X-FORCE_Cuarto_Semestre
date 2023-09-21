577189fd204ef6b66826ce49d140e93e6533c49b
EnzoCruzate <111470923+EnzoCruzate@users.noreply.github.com>
2023-09-20 21:49:57 -0300

    UpdateButton
    Agrego código de botón de cambio oscuro y claro

---
+++ Proyecto_E-Commerse/Front/Templates/js.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('.btn-neon-black');
+const img1 = document.querySelector('.fa-solid');
+const img2 = document.querySelector('.fa-solid');
+let isClicked = false;
+
+btn.addEventListener('click', () => {
+    if (!isClicked) {
+        img1.classList.add('fa-sun');
+        img1.classList.add('fa-moon');
+        img2.classList.remove('fa-moon');
+        img2.classList.add('fa-sun');
+        img2.style.color = '#e5a315';
+        document.documentElement.style.setProperty('--SombraNeon', '#eed48c');
+        document.documentElement.style.setProperty('--ColorPrimario', '#ececec');
+        document.documentElement.style.setProperty('--ColorTerciario', 'rgb(145, 3, 3)');
+        document.documentElement.style.setProperty('--FondoBody', '#0f0f0f');
+    } else {
+        img1.classList.remove('fa-moon');
+        img1.classList.add('fa-sun');
+        img2.classList.remove('fa-sun');
+        img2.classList.add('fa-moon');
+        // Restaura el color original
+        document.documentElement.style.setProperty('--SombraNeon', '#0e0024');
+        document.documentElement.style.setProperty('--ColorPrimario', 'rgb(20, 20, 20)')
+        document.documentElement.style.setProperty('--ColorTerciario', 'rgb(0, 217, 255)')
+        document.documentElement.style.setProperty('--FondoBody', '#ffffff');
+        img2.style.color = '#dbadf0';
+    }
+    isClicked = !isClicked;
+});
+
+
+--FondoBody
