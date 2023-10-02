0d26d739ccc624a146dbfa8f2a745eef1cb6c3a7
FacundoFlores9 <facutomas1@gmail.com>
2023-10-02 20:31:24 -0300

    [APLICACION][ShoppingCart_Y_Purchase] Se crearon las aplicaciones ShoppingCart y Purchase. Y se crearon los modelos ShoppingCart y Purchase.

---
+++ Proyecto_E-Commerse/ShopGods/Purchases/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Purchases(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    purchases_date = models.DateTimeField(auto_now_add=True)
+    card_data = models.TextField(null=True)
