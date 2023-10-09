ada76e01c373c6909fc75f51419ca6f1fee18564
jeroalvarez1 <jeroalvarez1@gmail.com>
2023-10-09 20:26:51 -0300

    [JAVA][SEMANA_03] Issue 46, 47, 48

---
+++ Metodologia_de_Sistemas/Java_Avanzado/Semana_03/EstudianteRepositorio.java
@@ -0,0 +1,7 @@
+package utn.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.estudiantes.modelo.Estudiante2022;
+
+public interface EstudianteRepositorio extends JpaRepository<Estudiante2022, Integer> {
+        }
---
+++ Metodologia_de_Sistemas/Java_Avanzado/Semana_03/IEstudianteServicio.java
@@ -0,0 +1,12 @@
+package utn.servicio;
+
+import utn.estudiantes.modelo.Estudiante2022;
+
+import java.util.List;
+
+public interface IEstudianteServicio {
+    public List<Estudiante2022> listarEstudiantes();
+    public Estudiante2022 buscarEstudiantePorId(Integer idEstudiante2022);
+    public void guardarEstudiante(Estudiante2022 estudiante);
+    public void eliminarEstudiante(Estudiante2022 estudiante);
+}
