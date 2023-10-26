8c0aa52a80c2abe8dd67f6e5c196d4c00b6ffddd
FacundoFlores9 <facutomas1@gmail.com>
2023-10-25 21:00:26 -0300

    [NODE][SEMANA_08] Issue 92

---
+++ Metodologia_de_Sistemas/Node/Semana_08/Fundametos02_node/async/callback.js
@@ -0,0 +1,22 @@
+function hola(nombre, miCallback){
+    setTimeout( function() {
+        console.log('Hola ' + nombre);
+        miCallback(nombre);
+    }, 1000);
+}
+
+
+function adios(nombre, otrocallback) {
+    setTimeout(function(){
+        console.log('Adiós '+ nombre);
+        otrocallback();
+    }, 1500);   
+}
+
+console.log('Iniciando el proceo...');
+
+hola('Carlos', function(nombre) {
+    adios(nombre, function(){
+        console.log('Terminando el proceso...');    
+    });
+});
