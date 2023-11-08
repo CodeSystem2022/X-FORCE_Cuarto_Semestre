db1a7824d1575d27e6b5499813b15bef5965083f
JuanVillagran <juanmvillagrand@gmail.com>
2023-11-08 09:33:35 -0300

    update templates

--- Proyecto_E-Commerse/ShopGods/ShopGods/urls.py
+++ Proyecto_E-Commerse/ShopGods/ShopGods/urls.py
@@ -1,9 +1,21 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from Core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/product/', include('Product.urls'))
-    
+    path("", views.home, name="login"),
+    path("RegisterUser/", views.registerUser, name="registerUser"),
+
+    path("Main/", views.main, name="main"),
+
+    path("User/",views.user, name="user"),
+    path("EditUser/", views.editUser, name="editUser"),
+
+
+#----------Rutas de la aplicación----------#
+    path('login/', include('Core.urls')),
+
+    path('test/product/', include('Product.urls')),
 ]
